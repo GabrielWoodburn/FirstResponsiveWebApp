165fac5141d057b041a43c1a5b5c8b52a2dcfd84
GabrielWoodburn <gwoodburn@dmacc.edu>
2021-02-01 04:20:28 -0600

    Initial Commit
    Initial Commit of the Age Calculator

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstResponsiveWebAppWoodburn.Models;
+
+namespace FirstResponsiveWebAppWoodburn.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Age = 0;
+            ViewBag.AgeAtEndOfYear = 0;
+            ViewBag.Name = "";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(AgeModel model)
+        {
+            if (ModelState.IsValid)  // Check valid state
+            {
+                ViewBag.Age = model.AgeThisYear();  // Call AgeThisYear() function and assign
+                ViewBag.AgeAtEndOfYear = model.AgeByEndOfYear();  // Call AgeByEndOfYear() function and assign
+                ViewBag.Name = model.Name;  // Assign name
+            }
+            else // Assign defaults
+            {
+                ViewBag.Age = 0;
+                ViewBag.AgeAtEndOfYear = 0;
+                ViewBag.Name = "";
+            }
+            return View(model); 
+        }
+    }
+}
---
+++ Models/AgeModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstResponsiveWebAppWoodburn.Models
+{
+    public class AgeModel
+    {
+        const int DAYS_IN_YEAR = 365;  // Const to hold days a in year
+        const int ONE_YEAR = 1;        // Const to hold value for one year
+
+        public string Name { get; set; }         // Name attribute
+
+        // Validation for BirthDate is on the label within the form
+        public DateTime BirthDate { get; set; }  // Birthdate attribute
+        public int AgeThisYear()  // Function to calculate current age
+        {
+            int age = DateTime.Now.Subtract(BirthDate).Days;  // Assign age
+            age = age / DAYS_IN_YEAR;  // Divide by 365
+            return age;  // Return age
+        }
+        public int AgeByEndOfYear() // Function to calculate age by December 31st
+        {
+            int age = DateTime.Now.Subtract(BirthDate).Days;  // Assign age
+            age = age / DAYS_IN_YEAR;  // Divide by 365
+            return age + ONE_YEAR;     // Return age + 1
+        }
+    }
+}
