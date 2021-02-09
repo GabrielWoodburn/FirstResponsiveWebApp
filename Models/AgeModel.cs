95cf328861156ac836098826154c217ad717046a
GabrielWoodburn <gwoodburn@dmacc.edu>
2021-02-09 01:31:27 -0600

    Additional Business Logic Added
    Business Logic added to handle birthdates correctly.

--- Models/AgeModel.cs
+++ Models/AgeModel.cs
@@ -9,6 +9,7 @@ namespace FirstResponsiveWebAppWoodburn.Models
     {
         const int DAYS_IN_YEAR = 365;  // Const to hold days a in year
         const int ONE_YEAR = 1;        // Const to hold value for one year
+        const int ZERO = 0;
 
         public string Name { get; set; }         // Name attribute
 
@@ -20,11 +21,39 @@ namespace FirstResponsiveWebAppWoodburn.Models
             age = age / DAYS_IN_YEAR;  // Divide by 365
             return age;  // Return age
         }
+
         public int AgeByEndOfYear() // Function to calculate age by December 31st
         {
             int age = DateTime.Now.Subtract(BirthDate).Days;  // Assign age
-            age = age / DAYS_IN_YEAR;  // Divide by 365
-            return age + ONE_YEAR;     // Return age + 1
+            if(age <= ZERO)
+            {
+                if(BirthDate == DateTime.Now)
+                {
+                    age = age / DAYS_IN_YEAR;  // Divide by 365
+                    return age + ONE_YEAR;     // Return age + 1
+                }
+                else if(BirthDate > DateTime.Now)
+                {
+                    age = age / DAYS_IN_YEAR;  // Divide by 365
+                    return age;
+                }
+
+                age = age / DAYS_IN_YEAR;  // Divide by 365
+                return age;
+            }
+            else
+            {
+                if(BirthDate.Month > DateTime.Now.Month && BirthDate.Day > DateTime.Now.Day )
+                {
+                    age = age / DAYS_IN_YEAR;  // Divide by 365
+                    return age + ONE_YEAR;     // Return age + 1
+                }
+                else
+                {
+                    age = age / DAYS_IN_YEAR;  // Divide by 365
+                    return age;     // Return age
+                }
+            }
         }
     }
 }
